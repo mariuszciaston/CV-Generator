e4e709b4a98c64ef0082871a7d2ba2a849128e76
Mariusz <casmar90@gmail.com>
2024-11-04 12:13:15 +0100

    set default button color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,10 @@
 import { ButtonTypes } from "./types";
 
-const Button: React.FC<ButtonTypes> = ({ text, className }) => {
+const Button: React.FC<ButtonTypes> = ({ text, className = "bg-gray-500" }) => {
   return (
     <button
       type="button"
-      className={`flex-1 text-nowrap rounded-md px-4 py-2 text-sm font-medium text-white ${className ? className : "bg-gray-500"}`}
+      className={`flex-1 text-nowrap rounded-md px-4 py-2 text-sm font-medium text-white ${className}`}
     >
       {text}
     </button>
